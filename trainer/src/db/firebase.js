338a0075b01aa61af301f5dae8e75b95d88375a8
Your Nameseba-toso <jsgtoso@gmail.com>
2021-08-09 12:47:29 -0300

    Feature: Sorting videos by category and excercises

--- trainer/src/db/firebase.js
+++ trainer/src/db/firebase.js
@@ -6,13 +6,7 @@ import 'firebase/auth';
 
 
 const firebaseApp = firebase.initializeApp ({
-    apiKey: "AIzaSyDakiQSqn1GyJsIuuEu9bN5YqCROtMhwVA",
-    authDomain: "personaltrainer-8747d.firebaseapp.com",
-    databaseURL: "https://personaltrainer-8747d-default-rtdb.firebaseio.com",
-    projectId: "personaltrainer-8747d",
-    storageBucket: "personaltrainer-8747d.appspot.com",
-    messagingSenderId: "702201037787",
-    appId: "1:702201037787:web:31d032e52b2c71bb541dfe"
+
   });
 
   

fe61c02dcf7dfeb0d9f7318fb162d7eb900317f6
Seba-Toso <jsgtoso@gmail.com>
2021-08-30 20:04:41 -0300

    New State: Update log-register function - APP BUILD

--- trainer/src/db/firebase.js
+++ trainer/src/db/firebase.js
@@ -8,23 +8,11 @@ const environment = 'production'
 
 const firebaseApp = environment === 'test' ?  
 firebase.initializeApp ({
-	apiKey: "AIzaSyAVraOQGECB5a8AyvySXNgouv9oQg2RBHY",
-	authDomain: "it-devs-testing-db.firebaseapp.com",
-	projectId: "it-devs-testing-db",
-  databaseURL: "https://it-devs-testing-db.firebaseio.com",
-	storageBucket: "it-devs-testing-db.appspot.com",
-	messagingSenderId: "175908001248",
-	appId: "1:175908001248:web:51738ab93f51a1261fc785"
+//Paste here firebase app test config
 })
 :
 firebase.initializeApp ({
-  apiKey: "AIzaSyDakiQSqn1GyJsIuuEu9bN5YqCROtMhwVA",
-  authDomain: "personaltrainer-8747d.firebaseapp.com",
-  databaseURL: "https://personaltrainer-8747d-default-rtdb.firebaseio.com",
-  projectId: "personaltrainer-8747d",
-  storageBucket: "personaltrainer-8747d.appspot.com",
-  messagingSenderId: "702201037787",
-  appId: "1:702201037787:web:31d032e52b2c71bb541dfe"
+//Paste here firebase app config
 });
 
 const db = firebase.firestore();

37b66a59fbb350e764e3e13636ffee8336912732
Seba-Toso <jsgtoso@gmail.com>
2023-01-13 19:46:12 -0300

    remove update ticket button

--- trainer/src/pages/Register.js
+++ trainer/src/pages/Register.js
@@ -174,14 +174,14 @@ const Register = ({alert}) => {
                     <div className="row">
                         <div className="col-lg-8  mx-auto">
                             <div className="leave-comment mt-5">
-                                {
+                                {/* {
                                 currentUser && currentUser.email !== 'admin@gmail.com' &&
                                 <div className="custom-control custom-switch">
                                     <input type="checkbox" className="custom-control-input" id="customSwitch1" onClick={() => setIsForUpdate(!isForUpdate)} />
                                     <label className="custom-control-label text-light mr-1" for="customSwitch1">Update</label>
                                     <small style={{color:"#444"}}> - Si tenes que actualizar el número de cupón de pago, hacé click en update</small>
                                 </div>
-                                }
+                                } */}
                                 <br/>
                                 {
                                 isForUpdate ?

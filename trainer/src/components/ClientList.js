5d1bc8f98fd5d0de322cada1528de96e960afd33
Seba-Toso <jsgtoso@gmail.com>
2023-01-14 14:45:11 -0300

    Add filters and collapsables to user administration. Change admin buttons

--- trainer/src/components/ClientList.js
+++ trainer/src/components/ClientList.js
@@ -1,167 +1,231 @@
-import React, { useState, useEffect, useMemo, memo, useCallback} from 'react';
+import React, { useState, useEffect, useMemo, memo, useCallback } from 'react';
 import { db } from '../db/firebase';
-import { IoCheckbox, IoCloseCircle } from 'react-icons/io5';
+import { IoCheckbox, IoCloseCircle, IoTrashBin } from 'react-icons/io5';
 
 
-const ClientList = () => {
-    const [data, setData] = useState([]);
-    const [isFetching, setIsfetching] = useState(false);
+const ClientList = ({filter = ''}) => {
+  const [data, setData] = useState([]);
+  const [isFetching, setIsfetching] = useState(false);
+  const [cardOpened, setCardOpened] = useState([])
 
 
+  useEffect(() => {
+    setIsfetching(true)
+    const clientsMessages = db.collection('Suscription').onSnapshot(snap => {
+      let data = snap.docs.map(doc => ({ ...doc.data(), 'id': doc.id }))
+      
+      if(!filter || filter === 'date' || filter === 'alphabetical'){
+        data.sort( (a, b) => {
+          let item
+          if(!filter || filter === 'date'){
+              item = 1
+          }
+          if(filter === 'alphabetical'){
+              if(a.firstName.toUpperCase() < b.firstName.toUpperCase()){item = -1}
+              else if(a.firstName.toUpperCase() > b.firstName.toUpperCase()){item = 1}
+              else{ item = 0}
+          }
+          return item
+        })
+        setData(data)
+      }else if(filter || filter !== 'date' || filter !== 'alphabetical'){
+        let users = data.filter(user =>  user.firstName.toUpperCase().includes(filter.toUpperCase()))
+        setData(users)
+      }else(setData(data))
+      
 
-    useEffect(() => { 
-        setIsfetching(true)
-        const clientsMessages = db.collection('Suscription').onSnapshot(snap => {
-            const data = snap.docs.map(doc => ({...doc.data(), 'id': doc.id}))
-            setData(data)
-            //console.log(data)
-            setIsfetching(false)
-        });
-        return () => clientsMessages()
+      setIsfetching(false)
+    });
+
+    return () => clientsMessages()
+  }, [filter]);
+
+  useEffect(() => {
+    if (data?.length) {
+      const cardOpenedLength = data.map(data => false)
+      setCardOpened(cardOpenedLength)
+    }
+  }, [data])
+
+  const handleDelete = (userIndex, userId) => {
+    db.collection('Suscription').doc(userId).delete()
+    //.then(() => window.location.reload())
+  }
+
+  const handleCancelSubscription = useCallback((userIndex, userId) => {
+    const user = data[userIndex]
+    const updatedUser = { ...user, payment: !user.payment }
+
+    db.collection('Suscription').doc(userId).set(updatedUser)
+  }, [data])
+
+  const openerHandler = useCallback((indx) => {
+    const updateCardOpened = cardOpened.map((data, index) => {
+      if(index === indx){
+        return !cardOpened[indx]
+      }
+      return false
+    })
+
+    setCardOpened(updateCardOpened)
+  },[cardOpened])
+
+  const messageList = useMemo(() => {
     
-    }, []);
-
-    const handleDelete = (userIndex, userId) => {
-        db.collection('Suscription').doc(userId).delete()
-        //.then(() => window.location.reload())
+    if (!data.length) {
+      return (
+        <div className="container  mt-5 mb-5 text-center">
+          <h2 style={{ color: 'White' }}>{!isFetching ? 'CARGANDO CLIENTES...' : 'AUN NO HAY CLIENTES'}</h2>
+        </div>
+      )
     }
 
-    const handleCancelSubscription = useCallback((userIndex, userId) => {
-        const user = data[userIndex]
-        const updatedUser = {...user, payment: !user.payment}
-
-        db.collection('Suscription').doc(userId).set(updatedUser)
-    },[data])
-
-    const messageList = useMemo(() => {
-        return !data.length ? (
-            <div className="container  mt-5 mb-5 text-center">
-            <h2 style={{color:'White'}}>{!isFetching? 'CARGANDO CLIENTES...' : 'AUN NO HAY CLIENTES'}</h2>
-            </div>
-        )
-        :
-        (
-            <div className="container">
-                {
-                    data.map( (doc, index) => {
-                        return (
-                            <div className="table" key={doc.id} >
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>PAGO </div>
-                                    <div className='tableDescription tableAction' onClick={() => handleCancelSubscription(index, doc.id)}>
-                                    {doc.payment ? <IoCheckbox color='#48ad5f' fontSize={18}/> : <IoCloseCircle color='#a03549' fontSize={18}/>} - {doc.payment ? 'Activo' : 'Inactivo'}
-                                    </div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>ID: </div>
-                                    <div className='tableDescription'>{doc.id}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>NOMBRE: </div>
-                                    <div className='tableDescription'>{doc.firstName}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>APELLIDO: </div>
-                                    <div className='tableDescription'>{doc.lastName}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>E-MAIL: </div>
-                                    <div className='tableDescription'>{doc.email}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>ANTECEDENTES MEDICOS: </div>
-                                    <div className='tableDescription'>{doc.medicalHistory}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>PESO: </div>
-                                    <div className='tableDescription'>{doc.weight}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>ALTURA: </div>
-                                    <div className='tableDescription'>{doc.height}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>EDAD: </div>
-                                    <div className='tableDescription'>{doc.age}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>CANTIDAD DE DIAS PARA ENTRENAR: </div>
-                                    <div className='tableDescription'>{doc.availableDays}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>LUGAR DE ENTRENAMIENTO: </div>
-                                    <div className='tableDescription'>{doc.trainingPlace}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>DISPONE DE ELEMENTOS? DETALLARLOS: </div>
-                                    <div className='tableDescription'>{doc.tools}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>OBJETIVO: </div>
-                                    <div className='tableDescription'>{doc.target}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>NUMERO DE CUPON DE OPERACION: </div>
-                                    <div className='tableDescription'>{doc.cuponNumber}</div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>FOTO DE FRENTE: </div>
-                                    <div className='tableDescription'>
-                                        <img 
-                                        src={doc.imageFront} 
-                                        alt="front"
-                                        width="100px"
-                                        />
-                                    </div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>FOTO DE ESPALDA: </div>
-                                    <div className='tableDescription'>
-                                        <img 
-                                        src={doc.imageBack} 
-                                        alt="front"
-                                        width="100px"
-                                        />
-                                    </div>
-                                </span>
-                                <span className='tableItem'>
-                                    <div className='tableTitle'>BORRAR USUARIO</div>
-                                    <div className='tableDescription tableAction' onClick={() => handleDelete(index, doc.id)}> 
-                                    <svg 
-                                        type="button" 
-                                        className=" bi bi-trash" 
-                                        data-toggle="modal" 
-                                        data-target="#exampleModal"
-                                        ns="http://www.w3.org/2000/svg" 
-                                        width="16" 
-                                        height="16" 
-                                        fill="currentColor" 
-                                        viewBox="0 0 16 16"
-                                    >
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                    </svg>                                        
-                                    </div>
-                                </span>
-                            </div>
-                        )
-                    })
-                }     
-            </div>
-        )
-    }, [ data, isFetching, handleCancelSubscription ])
-
-
-
-
-
     return (
-        <div>
-         <div className="Container projectSlider section">
-                { messageList }
-            </div>
+      <div className="container">
+        <div className="accordion" id="accordionExample"  >
+          {
+            data.map((doc, index) => {
+              return (
+                <div className="card" key={doc.id}>
+                  <div className="card-header" id={"heading-" + doc.firstName}>
+                    <h2 className="mb-0 d-flex">
+                      <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target={"#collapse-" + doc.firstName} aria-expanded="false" aria-controls={"collapse-" + doc.firstName} onClick={() => openerHandler(index)}>
+                        {doc.firstName + ' ' + doc.lastName}
+                      </button>
+                      {
+                        cardOpened[index] &&
+                        <>
+                          <button className="btn btn-success" type="button" onClick={() => handleCancelSubscription(index, doc.id)} style={{ margin: '0 5px' }}>
+                            {doc.payment ? <IoCheckbox fontSize={16} style={{ verticalAlign: 'baseline' }} /> : <IoCloseCircle fontSize={16} style={{ verticalAlign: 'baseline' }} />}
+                          </button>
+                          <button className="btn btn-danger" type="button" onClick={() => handleDelete(index, doc.id)}>
+                            <IoTrashBin fontSize={16} style={{ verticalAlign: 'baseline' }} />
+                          </button>
+                        </>
+                      }
+
+                    </h2>
+                  </div>
+
+                  <div id={"collapse-" + doc.firstName} className="collapse" aria-labelledby={"heading-" + doc.firstName} data-parent="#accordionExample">
+                    <div className="card-body">
+                      <div className="table" >
+                        {/* <span className='tableItem'>
+                            <div className='tableTitle'>HABILITAR </div>
+                            <div className='tableDescription tableAction' onClick={() => handleCancelSubscription(index, doc.id)}>
+                              {doc.payment ? <IoCheckbox color='#48ad5f' fontSize={18} /> : <IoCloseCircle color='#a03549' fontSize={18} />}
+                            </div>
+                          </span> */}
+                        <span className='tableItem'>
+                          <div className='tableTitle'>ID: </div>
+                          <div className='tableDescription'>{doc.id}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>NOMBRE: </div>
+                          <div className='tableDescription'>{doc.firstName}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>APELLIDO: </div>
+                          <div className='tableDescription'>{doc.lastName}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>E-MAIL: </div>
+                          <div className='tableDescription'>{doc.email}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>ANTECEDENTES MEDICOS: </div>
+                          <div className='tableDescription'>{doc.medicalHistory}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>PESO: </div>
+                          <div className='tableDescription'>{doc.weight}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>ALTURA: </div>
+                          <div className='tableDescription'>{doc.height}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>EDAD: </div>
+                          <div className='tableDescription'>{doc.age}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>DÍAS DE ENTRENAMIENTO: </div>
+                          <div className='tableDescription'>{doc.availableDays}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>LUGAR DE ENTRENAMIENTO: </div>
+                          <div className='tableDescription'>{doc.trainingPlace}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>DETALLE DE ELEMENTOS </div>
+                          <div className='tableDescription'>{doc.tools}</div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>OBJETIVO: </div>
+                          <div className='tableDescription'>{doc.target}</div>
+                        </span>
+                        {/* <span className='tableItem'>
+                            <div className='tableTitle'>NUMERO DE CUPON DE OPERACION: </div>
+                            <div className='tableDescription'>{doc.cuponNumber}</div>
+                          </span> */}
+                        <span className='tableItem'>
+                          <div className='tableTitle'>FOTO DE FRENTE: </div>
+                          <div className='tableDescription'>
+                            <img
+                              src={doc.imageFront}
+                              alt="front"
+                              width="100px"
+                            />
+                          </div>
+                        </span>
+                        <span className='tableItem'>
+                          <div className='tableTitle'>FOTO DE ESPALDA: </div>
+                          <div className='tableDescription'>
+                            <img
+                              src={doc.imageBack}
+                              alt="front"
+                              width="100px"
+                            />
+                          </div>
+                        </span>
+                        {/* <span className='tableItem'>
+                            <div className='tableTitle'>BORRAR USUARIO</div>
+                            <div className='tableDescription tableAction' onClick={() => handleDelete(index, doc.id)}>
+                              <svg
+                                type="button"
+                                className=" bi bi-trash"
+                                data-toggle="modal"
+                                data-target="#exampleModal"
+                                ns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                              </svg>
+                            </div>
+                          </span> */}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
-    );
+      </div>
+    )
+  }, [data, isFetching, handleCancelSubscription, cardOpened, openerHandler])
+
+  return (
+    <div>
+      <div className="Container projectSlider section">
+        {messageList}
+      </div>
+    </div>
+  );
 }
 
 export default memo(ClientList);
--- trainer/src/pages/Admin.js
+++ trainer/src/pages/Admin.js
@@ -1,71 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClientList from '../components/ClientList';
 import ClientMessage from '../components/ClientMessage';
 import AdminVideos from '../components/AdminVideos';
+import { IoMailOutline, IoPersonCircleOutline, IoPushOutline, IoClose, IoSearchCircle, IoText, IoCalendar } from 'react-icons/io5';
 import './Css/Admin.css';
 
 const Admin = () => {
+	const [open, setOpen] = useState(false)
+	const [filterBy, setFilterBy] = useState('')
 
-    return (
-        <div className="back-admin layoutContainer">
-            <section className="choseus-section spad" id="comments">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="section-title">
-                                <span>ADMINISTRADOR</span>
-                                <h2>MENSAJES DE CLIENTES</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <ClientMessage />
-            <section className="choseus-section spad" id="registers">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="section-title">
-                                <span>ADMINISTRADOR</span>
-                                <h2>CLIENTES REGISTRADOS</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <ClientList />
-            <section className="choseus-section spad" id="videos">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="section-title">
-                                <span>ADMINISTRADOR</span>
-                                <h2>ACTUALIZACION DE VIDEOS</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <AdminVideos />
-            <a className="text-center btn-flotante distanceOne" href="#comments">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16">
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
-                </svg>
-            </a>
-            <a className="text-center btn-flotante distanceTwo" href="#registers">
-               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                </svg>
-            </a>
-            <a className="text-center btn-flotante" href="#videos">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-play-btn" viewBox="0 0 16 16">
-            <path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
-            </svg>
-            </a>
-        </div>
-    );
+	const handleOpen = () => {
+		setOpen(prev => !prev)
+	}
+	const handleFilter = (filter) => {
+		setFilterBy(filter)
+		handleOpen()
+	}
+
+	return (
+		<div className="back-admin layoutContainer">
+			<section className="classes-section spad">
+				<div className="container d-flex dashboardContainer">
+					<div className="col-lg-2">
+						<div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+							<button className="nav-link active d-flex justify-content-between" id="v-pills-clientes-tab" data-toggle="pill" data-target="#v-pills-clientes" type="button" role="tab" aria-controls="v-pills-clientes" aria-selected="true">
+								Clientes <IoPersonCircleOutline fontSize={20} style={{ verticalAlign: 'baseline' }} />
+							</button>
+							<button className="nav-link d-flex justify-content-between" id="v-pills-mensajes-tab" data-toggle="pill" data-target="#v-pills-mensajes" type="button" role="tab" aria-controls="v-pills-mensajes" aria-selected="false">
+								Mensajes<IoMailOutline fontSize={20} style={{ verticalAlign: 'baseline' }} />
+							</button>
+							<button className="nav-link d-flex justify-content-between" id="v-pills-videoload-tab" data-toggle="pill" data-target="#v-pills-videoload" type="button" role="tab" aria-controls="v-pills-videoload" aria-selected="false">
+								Video nuevo<IoPushOutline fontSize={20} style={{ verticalAlign: 'baseline' }} />
+							</button>
+						</div>
+					</div>
+					<div className="col-lg-10">
+						<div className="tab-content" id="v-pills-tabContent">
+
+							<div className="tab-pane fade show active" id="v-pills-clientes" role="tabpanel" aria-labelledby="v-pills-clientes-tab">
+								<div className="row">
+									<div className="col">
+										<div className="section-title">
+											<span>ADMINISTRADOR</span>
+											<h2>CLIENTES REGISTRADOS</h2>
+										</div>
+									</div>
+								</div>
+								<ClientList filter={filterBy} />
+							</div>
+
+							<div className="tab-pane fade" id="v-pills-mensajes" role="tabpanel" aria-labelledby="v-pills-mensajes-tab">
+								<div className="row">
+									<div className="col">
+										<div className="section-title">
+											<span>ADMINISTRADOR</span>
+											<h2>MENSAJES DE CLIENTES</h2>
+										</div>
+									</div>
+								</div>
+								<ClientMessage />
+							</div>
+
+							<div className="tab-pane fade" id="v-pills-videoload" role="tabpanel" aria-labelledby="v-pills-videoload-tab">
+								<div className="row">
+									<div className="col">
+										<div className="section-title">
+											<span>ADMINISTRADOR</span>
+											<h2>ACTUALIZACION DE VIDEOS</h2>
+										</div>
+									</div>
+								</div>
+								<AdminVideos />
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<div className='filters d-flex'>
+				<IoSearchCircle className={open ? 'filter active' : 'filter'} fontSize={40} onClick={() => { handleOpen() }} />
+				<div className={open ? 'fadeIn' : 'fadeOff'}>
+					<IoCalendar className='filter' fontSize={36} onClick={() => { handleFilter('date') }} />
+					<IoText className='filter' fontSize={36} onClick={() => { handleFilter('alphabetical') }} />
+					<div className='filter' style={{ paddingBottom: '5px', borderBottom: '1px solid #F36100', margin: '0 auto' }}>
+						<input
+							type="text"
+							className='videoSearchInput'
+							onChange={(e) => setFilterBy(e.target.value)}
+							placeholder="Busca un usuario"
+							style={{
+								width: '250px',
+								marginTop: '9px',
+								backgroundColor: 'transparent',
+								outline: 'none',
+								border: 'none',
+								borderRadius: '0',
+								color: 'white',
+								boxShadow: 'none'
+							}}
+							value={filterBy && filterBy !== 'date' && filterBy !== 'alphabetical' ? filterBy : ''}
+						/>
+						{
+							filterBy && filterBy !== 'date' && filterBy !== 'alphabetical' && filterBy.length > 0 &&
+							<IoClose
+								onClick={() => { setFilterBy('') }}
+								style={{
+									cursor: 'pointer',
+								}}
+							/>
+						}
+					</div>
+				</div>
+			</div>
+
+		</div>
+	);
 }
 
 export default Admin;
